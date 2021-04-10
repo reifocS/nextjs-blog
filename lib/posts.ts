5bf3c551569bd3681583b1d14c5f5561c6a9cea3
reifocs <alainvincentesc@gmail.com>
2021-04-10 16:28:18 +0200

    typescript

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,31 +6,6 @@ import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
-export function getAllPostIds() {
-    const fileNames = fs.readdirSync(postsDirectory)
-
-    // Returns an array that looks like this:
-    // [
-    //   {
-    //     params: {
-    //       id: 'ssg-ssr'
-    //     }
-    //   },
-    //   {
-    //     params: {
-    //       id: 'pre-rendering'
-    //     }
-    //   }
-    // ]
-    return fileNames.map(fileName => {
-        return {
-            params: {
-                id: fileName.replace(/\.md$/, '')
-            }
-        }
-    })
-}
-
 export function getSortedPostsData() {
     // Get file names under /posts
     const fileNames = fs.readdirSync(postsDirectory)
@@ -48,7 +23,7 @@ export function getSortedPostsData() {
         // Combine the data with the id
         return {
             id,
-            ...matterResult.data
+            ...(matterResult.data as { date: string; title: string })
         }
     })
     // Sort posts by date
@@ -61,7 +36,18 @@ export function getSortedPostsData() {
     })
 }
 
-export async function getPostData(id) {
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory)
+    return fileNames.map(fileName => {
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, '')
+            }
+        }
+    })
+}
+
+export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -78,6 +64,6 @@ export async function getPostData(id) {
     return {
         id,
         contentHtml,
-        ...matterResult.data
+        ...(matterResult.data as { date: string; title: string })
     }
 }
